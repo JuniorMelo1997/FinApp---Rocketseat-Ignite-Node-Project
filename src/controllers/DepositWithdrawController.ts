dc70e2915004bf172f1d0b00184ef4f279217edf
Genival Melo Junior <genivaldemelojr@gmail.com>
2023-02-01 23:19:36 -0300

    withdraw

--- src/controllers/DepositWithdrawController.ts
+++ src/controllers/DepositWithdrawController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { compare } from "bcrypt";
 
-export class DepositController{
+export class DepositWithdrawController{
     async deposit(req:Request, res:Response){
         const {cpf, password, transaction} = req.body;
         const prismaClient = new PrismaClient();
@@ -29,6 +29,44 @@ export class DepositController{
             const statement = await prismaClient.statement.create({
                 data:{
                     transaction: Number(transaction),
+                    ownerId: client.id
+                }
+            })
+
+            return res.json(statement);
+        } catch (error) {
+            console.log(error);
+            return res.json({message: "Não foi possível realizar o depósito"});
+        }
+    }
+
+    async withdraw(req:Request, res:Response){
+        const {cpf, password, transaction} = req.body;
+        const prismaClient = new PrismaClient();
+
+        const withdrawVal = transaction*(-1);
+        const client = await prismaClient.client.findFirst({where:{cpf:cpf}});
+
+        if(!client){
+            return res.json({message: "Cliente não encontrado"});
+        }
+
+        const passwordMatches = await compare(password, client.password);
+
+        if(!passwordMatches){
+            return res.json({message: "Senha incorreta"});
+        }
+
+        try {    
+            const newBalance = Number(client.balance) + Number(withdrawVal);         
+            const updateBalance = await prismaClient.client.update({
+                where: {cpf: client.cpf},
+                data: {balance: newBalance}
+            })
+
+            const statement = await prismaClient.statement.create({
+                data:{
+                    transaction: Number(withdrawVal),
                     ownerId: client.id
                 }
             })
--- src/routes.ts
+++ src/routes.ts
@@ -2,18 +2,19 @@ import express from "express";
 import { CreateClientController } from "./controllers/CreateClientController";
 import { GetBalanceController } from "./controllers/GetBalanceController";
 import { GetStatementController } from "./controllers/GetStatementController";
-import { DepositController } from "./controllers/DepositController";
+import { DepositWithdrawController } from "./controllers/DepositWithdrawController";
 
 const routes = express.Router();
 
 const createClientController = new CreateClientController();
 const getBalanceController = new GetBalanceController();
 const getStatementController = new GetStatementController();
-const depositController = new DepositController();
+const depositWithdrawController = new DepositWithdrawController();
 
 routes.post("/create", createClientController.createClient);
 routes.get("/balance", getBalanceController.getBalance);
 routes.get("/statement", getStatementController.getStatement);
-routes.post("/deposit", depositController.deposit);
+routes.post("/deposit", depositWithdrawController.deposit);
+routes.post("/withdraw", depositWithdrawController.withdraw);
 
 export {routes};
