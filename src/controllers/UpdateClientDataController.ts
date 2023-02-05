a36f69c3e7ad85be2a4769b101bdecc9b2078193
Genival Melo Junior <genivaldemelojr@gmail.com>
2023-02-04 23:06:03 -0300

    update client

---
+++ src/controllers/UpdateClientDataController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { compare, hash } from "bcrypt";
+
+export class UpdateClientDataController{
+    async updateClientData(req: Request, res: Response){
+        const prismaClient = new PrismaClient();
+        const {name, cpf, newCpf, password, newPassword, balance} = req.body;
+
+        const client = await prismaClient.client.findFirst({where:{
+            cpf: cpf
+        }});
+
+        if(!client){
+            return res.json({message: "CPF não cadastrado"});
+        }
+
+        const passwordMatches = await compare(password, client.password);
+
+        if(!passwordMatches){
+            return res.json({message: "Senha incorreta"});
+        }
+
+        try {
+            const hashedPassword = await hash(newPassword, 8);
+            const clientUpdated = await prismaClient.client.update({
+                where:{
+                    cpf: client.cpf
+                },
+                data:{
+                    name: name,
+                    password: hashedPassword,
+                    balance: balance
+                }
+            })
+
+            return res.json({message: "Dados do cliente atualizados com sucesso"});
+        } catch (error) {
+            return res.json({message: "Não foi possível atualizar os dados do cliente"});
+        }
+
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { CreateClientController } from "./controllers/CreateClientController";
 import { GetBalanceController } from "./controllers/GetBalanceController";
 import { GetStatementController } from "./controllers/GetStatementController";
 import { DepositWithdrawController } from "./controllers/DepositWithdrawController";
+import { UpdateClientDataController } from "./controllers/UpdateClientDataController";
 
 const routes = express.Router();
 
@@ -10,11 +11,13 @@ const createClientController = new CreateClientController();
 const getBalanceController = new GetBalanceController();
 const getStatementController = new GetStatementController();
 const depositWithdrawController = new DepositWithdrawController();
+const updateClientDataController = new UpdateClientDataController();
 
 routes.post("/create", createClientController.createClient);
 routes.get("/balance", getBalanceController.getBalance);
 routes.get("/statement", getStatementController.getStatement);
 routes.post("/deposit", depositWithdrawController.deposit);
 routes.post("/withdraw", depositWithdrawController.withdraw);
+routes.post("/client/update", updateClientDataController.updateClientData);
 
 export {routes};
